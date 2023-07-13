b40f47d2df64478ad3f001256ac4936070da1103
Njikem <123434988+Njikem@users.noreply.github.com>
2023-07-13 06:09:09 -0600

    variables

---
+++ js/app.js
@@ -0,0 +1,24 @@
+let user_points = 75;
+if(user_points >=75){
+    console.log("congratulation user ponits is enough");
+}else{
+    console.log("user points is not enough ");
+}
+
+let is_member = true;
+if(is_member =true && user_points >=90){
+    console.log(" ur points are enough");
+
+}else{
+    console.log("sorry your points are not enough");
+}
+
+let username = "Nasi";
+if(username = Nasi){
+    console.log("hello Alex, nice meeting you");
+}else{
+    console.log("Welcome");
+}
+
+
+
